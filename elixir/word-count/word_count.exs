55240245089267c05b6814cc90cb43579aae19d7
Pancia <anthony.dayzerostudio@gmail.com>
2015-01-02 19:18:04 -0800

    Minor changes, new regex (a lot cleaner).

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -1,22 +1,19 @@
 defmodule Words do
-  def assoc(key, map, default \\ 0) do
-    Map.put(map, key, Map.get(map, key, default) + 1)
+  def inc_freq(key, map) do
+    Map.update(map, key, 1, &(&1+1))
   end
 
   def frequencies(coll) do
-    coll |> List.foldr(%{}, &assoc/2)
+    coll |> List.foldr(%{}, &inc_freq/2)
   end
 
   @spec count(String.t) :: map()
   def count(sentence) do
-    #Could not get \W to work with -
-    punc = ~r/[\~\`\!\@\#\$\%\^\&\*
-               \(\)\_\+\=\;\'\:\"\{
-               \|\;\}\<\>\,\.\/\?\_]/u
+    unclean = ~r/[^\w-]|_/u
     sentence
-    |> String.replace(punc, " ")
-    |> String.downcase
-    |> String.split
-    |> frequencies
+      |> String.downcase
+      |> String.replace(unclean, " ")
+      |> String.split
+      |> frequencies
   end
 end
